5784a343070fbe2120be6a177654e1e25c5329d8
telepuzik <elxgato@gmail.com>
2014-09-09 14:12:11 +0400

    f

--- clientjs/load.js
+++ clientjs/load.js
@@ -1,5 +1,15 @@
 /**
  * Created by  on 09.09.2014.
  */
-
 console.log ("loaded");
+window.onload = function () {
+    var mainLabel = document.getElementById("mainLabel");
+    mainLabel.innerHTML = "Loaded";
+    //console.log ("loaded");
+    var words = GetWordsClient();
+    console.log(words);
+};
+
+function GetWords(){
+
+}
